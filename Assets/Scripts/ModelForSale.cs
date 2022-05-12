d8561407ac4213c647eb85720cecfaf33f138b6d
Yana-commits <74359419+Yana-commits@users.noreply.github.com>
2022-05-12 11:25:07 +0300

    complex building prefab

--- Assets/Scripts/ModelForSale.cs
+++ Assets/Scripts/ModelForSale.cs
@@ -16,9 +16,8 @@ public class ModelForSale : MonoBehaviour
     }
     public void Init(GameObject model)
     {
-      
        model.transform.rotation = Quaternion.Euler(0, 90, 0);
-        model.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        model.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
 
         model.transform.SetParent(this.gameObject.transform);
         model.transform.localPosition = new Vector3(0, 0, 0);
